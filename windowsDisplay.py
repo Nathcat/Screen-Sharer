acc38ee04a425b01e23b2154b7b652ed301dc8c1
Nathcat <supernath126@gmail.com>
2021-03-01 16:34:35 +0000

    Added support for windows devices.

---
+++ windowsDisplay.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+
+class Application:
+    def display(self, img):
+        img = np.array(img)
+        # Convert RGB to BGR
+        img = img[:, :, ::-1].copy()
+        cv2.imshow("Screen sharer", img)
